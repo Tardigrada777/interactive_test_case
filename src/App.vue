c00b0ad6004c92aa98e4dcff4c7a0d6c076c15e0
Vladimir Myltsev <ntwrite@gmail.com>
2020-04-24 11:53:17 +0300

    feat: setup pug and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<template lang="pug">
+  .app.bg-gray-300.h-screen
+    .container.mx-auto.p-6
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>
